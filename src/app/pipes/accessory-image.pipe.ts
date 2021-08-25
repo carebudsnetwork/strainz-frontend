2f830eb4bde2f8fcc9a3337576cb348f752bf108
derekryansnzy <derekryans@notzeroyet.co>
2021-08-25 00:10:15 -0400

    Resized accessory pictures

--- src/app/pipes/accessory-image.pipe.ts
+++ src/app/pipes/accessory-image.pipe.ts
@@ -9,12 +9,12 @@ export class AccessoryImagePipe implements PipeTransform {
   transform(value: AccessoryType, ...args: unknown[]): string {
     switch (value) {
       case AccessoryType.Sunglasses:
-        return 'assets/sunglasses.png';
+        return 'assets/sunglasses-h.png';
       case AccessoryType.Joint:
-        return 'assets/joint.png';
+        return 'assets/joint-h.png';
 
       case AccessoryType.Earring:
-        return 'assets/earring.png';
+        return 'assets/earring-h.png';
 
     }
   }
